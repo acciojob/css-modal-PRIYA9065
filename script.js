a84f4e32e01538b46ce99cbb7026cc24421228f1
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-02-01 17:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const btn = document.getElementById("openModal");
+const modal = document.getElementByquerySelector("modal");
+const closebtn = document.getElementByquerySelector("close-modal");
+
+btn.onclick = function(){
+	modal.style.display = "block";
+}
+
+closebtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
